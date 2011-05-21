8368cd015c3b025614be8b2ee1a2c47c8fe14009
PowerMugen <corentin.calvez@gmail.com>
2011-05-21 18:12:34 +0200

    * modification de la structure général - ajout du dossier public pour la protection des fichiers sur le serveur

--- config/params.php
+++ config/params.php
@@ -23,7 +23,7 @@ define ('FILEROOT', $_SERVER["DOCUMENT_ROOT"] . $siteInfos['base_url']);
 define ('WEBROOT', 'http://' . $_SERVER["HTTP_HOST"] . '/' . $siteInfos['base_url']);
 
 // path to storage
-define ('UPLOADROOT', $_SERVER["DOCUMENT_ROOT"] . 'modules/storage/');
+define ('UPLOADROOT', $_SERVER["DOCUMENT_ROOT"] . 'storage/');
 
 // path to admin
 define ('ADMINROOT', WEBROOT . 'admin/');
