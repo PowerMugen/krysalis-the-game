8eeaa35cb034ef6007c333686112338240da30d7
AdrienGiboire <adrien.giboire@gmail.com>
2011-05-22 18:14:28 +0200

    Suppression des instructions de debug

--- modules/content.php
+++ modules/content.php
@@ -26,34 +26,5 @@ if (!file_exists($path = FILEROOT.'/modules/'.$module.'/'.$page.'.php')) {
 	$path = FILEROOT.'/404.php';
 }
 
-echo 'include '.$module.'/'.$page;
-
-/* our modules > the pages inside each module
-
-
-//case where page is defined but not module
-if (isset($modules['pages'][$page])) {
-	$modulePath = '';
-	$pageFile = $modules['pages'][$page];
-}
-//case where module is defined but page is not in array
-if (isset($modules['modules'][$module]) && !empty($page) && !in_array($page, $modules['modules'][$module]['pages']))  {
-	$modulePath =  'modules/';
-	$pageFile = '404.php';
-}
-//case where module and page are defined
-if (isset($modules['modules'][$module]) && isset($modules['modules'][$module]['pages'][$page])) {
-	$modulePath =  'modules/' . $modules['modules'][$module]['modulePath'];
-	$pageFile = $modules['modules'][$module]['pages'][$page];
-}
-
-// appel du fichier à inclure
-if (isset($modulePath) && isset($pageFile)) {
-	$file = FILEROOT . $modulePath . $pageFile;
-} else {
-	$file = FILEROOT . 'modules/404.php';
-}
-*/
-
 // include du fichier correspondant
 include $path;
