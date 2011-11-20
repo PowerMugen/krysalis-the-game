85aeaa3e96b135503744dc08952b1da815e4a174
jean-christian dubois <jean-christian.dubois@hotmail.fr>
2011-11-21 00:40:38 +0100

    MOD reparation fichier histoire

--- modules/krysalis/index.php
+++ modules/krysalis/index.php
@@ -1,8 +1,11 @@
 <div id="content">
-<h2 class="krysalis">l'histoire</h2>
-<p class="krysalis">
-<em>« 3 mars 2019 : notre équipe a interrompu la simulation de vie sur Mars qu’elle menait depuis bientôt deux ans, enfermée dans une station de la Zone 51. Notre QG ne répond plus. Toutes les fréquences sont muettes. Partout où nous allons, nous ne trouvons que des cadavres. Et pire encore. Ils ont envahi notre monde, et leurs intentions sont hostiles. Qui sont-ils ? D’où viennent-ils ? Mais surtout : combien de temps tiendrons-nous ? »</em><br/><br/>
-<strong>Krysalis</strong> est un jeu de plateau mettant en scène une équipe de 12 scientifiques, civils et militaires, plongés au cœur du chaos : une mystérieuse catastrophe a balayé la race humaine tandis qu’ils étaient coupés du monde, et voilà maintenant qu’ils doivent faire face à l’invasion de créatures mystérieuses, dangereuses, rapides et sans aucune pitié. Alors même qu’ils doivent lutter pour survivre, fuyant les zones infestées et traversant les États-Unis à la recherche d’éventuels rescapés, ils cherchent à comprendre l’origine des envahisseurs…<br/><br/>
-Des rues ensablées de Las Vegas à la chaleur du désert des Mojaves, en passant par Los Angeles en proie aux incendies, plongez dans un enfer post-apocalyptique où l’humain n’a plus droit de cité, et où vos seuls armes seront vos rares munitions, votre détermination et l’aide de vos compagnons d’infortune.
-</p>
-<a href="../krysalis/jouer.html" class="suivant">Apprendre comment jouer</a>
+        <div id="texte">
+            <h2 class="krysalis">l'histoire</h2>
+            <p class="krysalis">
+            <em>« 3 mars 2019 : notre équipe a interrompu la simulation de vie sur Mars qu’elle menait depuis bientôt deux ans, enfermée dans une station de la Zone 51. Notre QG ne répond plus. Toutes les fréquences sont muettes. Partout où nous allons, nous ne trouvons que des cadavres. Et pire encore. Ils ont envahi notre monde, et leurs intentions sont hostiles. Qui sont-ils ? D’où viennent-ils ? Mais surtout : combien de temps tiendrons-nous ? »</em><br/><br/>
+            <strong>Krysalis</strong> est un jeu de plateau mettant en scène une équipe de 12 scientifiques, civils et militaires, plongés au cœur du chaos : une mystérieuse catastrophe a balayé la race humaine tandis qu’ils étaient coupés du monde, et voilà maintenant qu’ils doivent faire face à l’invasion de créatures mystérieuses, dangereuses, rapides et sans aucune pitié. Alors même qu’ils doivent lutter pour survivre, fuyant les zones infestées et traversant les États-Unis à la recherche d’éventuels rescapés, ils cherchent à comprendre l’origine des envahisseurs…<br/><br/>
+            Des rues ensablées de Las Vegas à la chaleur du désert des Mojaves, en passant par Los Angeles en proie aux incendies, plongez dans un enfer post-apocalyptique où l’humain n’a plus droit de cité, et où vos seuls armes seront vos rares munitions, votre détermination et l’aide de vos compagnons d’infortune.
+            </p>
+            <a href="../krysalis/jouer.html" class="suivant">Apprendre comment jouer</a>
+	</div>
+</div>
